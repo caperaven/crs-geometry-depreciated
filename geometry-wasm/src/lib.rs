a96bd5b517dfeb7b42209da35fa224090cc0ae6c
caperaven <caperaven@gmail.com>
2021-08-28 15:18:19 +0200

    finish exporting rectangle to wasm

--- geometry-wasm/src/lib.rs
+++ geometry-wasm/src/lib.rs
@@ -4,16 +4,24 @@ pub extern crate crs_geometry;
 
 use wasm_bindgen::prelude::*;
 use js_sys::Object;
-use crate::utils::{set_property, set_point_array_property, set_int_array_property};
+use crate::utils::{
+    set_property,
+    set_point_array_property,
+    set_int_array_property,
+    set_point_property,
+    set_aabb_property
+};
 
 #[wasm_bindgen]
 pub fn rectangle(name: String, x: i32, y: i32, width: i32, height: i32) -> Object {
     let shape = crs_geometry::rectangle(name, x, y, width, height);
     let result = Object::new();
 
-    set_property(&result, &"name".into(), &shape.name.into());
+    set_property(&result, "name", &shape.name.into());
     set_point_array_property(&result, "points", &shape.points);
     set_int_array_property(&result, "indices", &shape.indices);
+    set_point_property(&result, "origin", &shape.origin);
+    set_aabb_property(&result, "aabb", &shape.aabb);
 
     return result;
 }
--- geometry-wasm/src/utils.rs
+++ geometry-wasm/src/utils.rs
@@ -1,8 +1,8 @@
 use wasm_bindgen::JsValue;
-use crs_geometry::Point;
+use crs_geometry::{Point, BoundingBox};
 
-pub fn set_property(obj: &js_sys::Object, property: &JsValue, value: &JsValue) {
-    js_sys::Reflect::set(obj, property, value).ok();
+pub fn set_property(obj: &js_sys::Object, property: &str, value: &JsValue) {
+    js_sys::Reflect::set(obj, &JsValue::from(property), value).ok();
 }
 
 pub fn set_point_array_property(obj: &js_sys::Object, property: &str, collection: &Vec<Point>) {
@@ -11,7 +11,8 @@ pub fn set_point_array_property(obj: &js_sys::Object, property: &str, collection
         values.push(&item.x.into());
         values.push(&item.y.into());
     }
-    js_sys::Reflect::set(obj, &JsValue::from(property), &values).ok();
+
+    set_property(obj, property, &values);
 }
 
 pub fn set_int_array_property(obj: &js_sys::Object, property: &str, collection: &Vec<i32>) {
@@ -21,5 +22,23 @@ pub fn set_int_array_property(obj: &js_sys::Object, property: &str, collection: 
         values.push(&JsValue::from(*item));
     }
 
-    js_sys::Reflect::set(obj, &JsValue::from(property), &values).ok();
+    set_property(obj, property, &values);
+}
+
+pub fn set_point_property(obj: &js_sys::Object, property: &str, point: &Point) {
+    let origin = js_sys::Object::new();
+    set_property(&origin, "x", &point.x.into());
+    set_property(&origin, "y", &point.y.into());
+    set_property(obj, property, &origin);
+}
+
+pub fn set_aabb_property(obj: &js_sys::Object, property: &str, aabb: &BoundingBox) {
+    let aabb_value = js_sys::Object::new();
+    set_property(&aabb_value, "min_x", &JsValue::from(aabb.min_x));
+    set_property(&aabb_value, "max_x", &JsValue::from(aabb.max_x));
+    set_property(&aabb_value, "min_y", &JsValue::from(aabb.min_y));
+    set_property(&aabb_value, "max_y", &JsValue::from(aabb.max_y));
+    set_property(&aabb_value, "width", &JsValue::from(aabb.width));
+    set_property(&aabb_value, "height", &JsValue::from(aabb.height));
+    set_property(obj, property, &aabb_value);
 }
